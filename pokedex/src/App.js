8ed7dd1c29f7a6960b92a2404c7d0d5557a0bbdc
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-06-16 17:39:49 -0300

    erro pendente

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,13 +1,14 @@
 import { MainContainer } from "./styled"
 import React from "react";
 import Router from "./Router/Router"
+import GlobalState from './global/GlobalState'
 
 const App = () => {
 
   return (
-    <MainContainer>
+    <GlobalState>
       <Router />
-    </MainContainer>
+    </GlobalState>
 
   )
 }
