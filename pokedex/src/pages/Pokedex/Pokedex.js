00c136afeb2c05a2186f16ab8f5ec9d79781a932
júllia Izidorio <contato.julliaizidorio@gmail.com>
2021-06-16 15:45:51 -0300

    card universal

--- pokedex/src/pages/Pokedex/Pokedex.js
+++ pokedex/src/pages/Pokedex/Pokedex.js
@@ -1,10 +1,13 @@
+import Card from "../../Components/Cards";
+import { Cards } from "./styled";
+
 const Pokedex = () => {
 
     return (
         <div>
             <h1>Pokedex</h1>
+            <Card />
         </div>
-
     )
 }
 
---
+++ pokedex/src/pages/Pokedex/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const Cards = styled.div`
+display:grid;
+border-radius:16px;
+border :2px solid black;
+justify-content:center;
+`
